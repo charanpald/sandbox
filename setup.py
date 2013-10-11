83628b7e731d40283ff69638cbb09eaaff94db49
Charanpal Dhanjal <charanpal@gmail.com>
2013-10-11 16:51:50 +0200

    Add cython modules

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 import os
+import numpy
 from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [Extension("sandbox.predictors.TreeCriterion", ["sandbox/predictors/TreeCriterion.pyx"]),
+    Extension("sandbox.util.SparseUtilsCython", ["sandbox/util/SparseUtilsCython.pyx"], include_dirs=[numpy.get_include()]),
+    Extension("sandbox.recommendation.SGDNorm2RegCython", ["sandbox/recommendation/SGDNorm2RegCython.pyx"], include_dirs=[numpy.get_include()]), 
+]
 
 setup(
     name = "sandbox",
     version = "0.1",
     author = "Charanpal Dhanjal ",
     author_email = "charanpal@gmail.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
+    description = ("A collection of machine learning algorithms"),
     license = "GPLv3",
     keywords = "numpy",
     url = "http://packages.python.org/sandbox",
@@ -17,5 +24,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License"
-    ],
+    ],  
+    cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
 )
