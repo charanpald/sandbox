6af34f2f8239cf32d81ab55cef2c76ed23b2a2d5
Charanpal Dhanjal <charanpal@gmail.com>
2013-10-23 17:26:23 +0200

    Overwrite file

--- sandbox/predictors/ABCSMC.py
+++ sandbox/predictors/ABCSMC.py
@@ -35,7 +35,7 @@ def loadThetaArray(N, thetaDir, t):
                 badFileDir = thetaDir + "debug/"
                 if not os.path.exists(badFileDir): 
                     os.mkdir(badFileDir)
-                shutil.rename(fileName, badFileDir + fileName)
+                shutil.move(fileName, badFileDir + fileName)
                 logging.warn("Moved " + fileName + " to " + badFileDir)
             except:
                 logging.error("Unexpected error whilst loading " + fileName)
