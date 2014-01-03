667e44d0db835e3836411db1a15a8d7aeaff731a
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-03 16:29:21 +0000

    Return c

--- sandbox/data/ExamplesGenerator.py
+++ sandbox/data/ExamplesGenerator.py
@@ -9,7 +9,7 @@ class ExamplesGenerator():
     def __init__(self):
         pass
     
-    def generateBinaryExamples(self, numExamples=100, numFeatures=10, noise=0.4):
+    def generateBinaryExamples(self, numExamples=100, numFeatures=10, noise=0.4, verbose=False):
         """
         Generate a certain number of examples with a uniform distribution between 0 and 1. Create
         binary -/+ 1 labels. Must have more than 1 example and feature. 
@@ -19,10 +19,14 @@ class ExamplesGenerator():
 
         X = numpy.random.rand(numExamples, numFeatures)
         c = numpy.random.rand(numFeatures)
+        
         y = numpy.sign((X.dot(c)) - numpy.mean(X.dot(c)) + numpy.random.randn(numExamples)*noise)
         y = numpy.array(y, numpy.int)
-
-        return X, y
+        
+        if not verbose: 
+            return X, y
+        else: 
+            return X, y, c
 
     def generateRandomBinaryExamples(self, numExamples=100, numFeatures=10):
         """
