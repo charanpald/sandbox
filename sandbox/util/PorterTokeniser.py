a49b13633035f2390d2e747689446d12dd5c7299
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-08 18:02:34 +0100

    Remove need for PyStemmer

--- sandbox/util/PorterTokeniser.py
+++ sandbox/util/PorterTokeniser.py
@@ -1,14 +1,23 @@
-import Stemmer
+
 import string
 
 #Tokenise the documents                 
 class PorterTokeniser(object):
     def __init__(self):
-        self.stemmer = Stemmer.Stemmer('english')
+        try:
+            import Stemmer 
+            self.stemmer = Stemmer.Stemmer('english')
+        except ImportError: 
+           print("Stemmer module not found")     
         self.minWordLength = 2
      
     def __call__(self, doc):
         doc = doc.lower().encode('utf-8').strip()
         doc = doc.translate(string.maketrans("",""), string.punctuation).decode("utf-8")
-        tokens =  [self.stemmer.stemWord(t) for t in doc.split()]  
+        try: 
+            import Stemmer        
+            tokens =  [self.stemmer.stemWord(t) for t in doc.split()]  
+        except ImportError: 
+            tokens =  [t for t in doc.split()] 
+            
         return [token for token in tokens if len(token) >= self.minWordLength]
