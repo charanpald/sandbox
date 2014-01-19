e3de4cd8f57c105a60acce153b8576def8acaa19
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-19 16:22:46 +0100

    Compute V in one go

--- sandbox/recommendation/MaxLocalAUCCython.pyx
+++ sandbox/recommendation/MaxLocalAUCCython.pyx
@@ -284,3 +284,84 @@ def derivativeVi(X, numpy.ndarray[double, ndim=2, mode="c"] U, numpy.ndarray[dou
     deltaPhi -= deltaAlpha
     
     return deltaPhi
+    
+@cython.boundscheck(False)
+def derivativeVApprox(X, numpy.ndarray[double, ndim=2, mode="c"] U, numpy.ndarray[double, ndim=2, mode="c"] V, omegaList, numpy.ndarray[long, ndim=1, mode="c"] indsJ, unsigned int sampleSize, unsigned int k, double lmbda, numpy.ndarray[double, ndim=1, mode="c"] r): 
+    """
+    delta phi/delta V using a few randomly selected rows of V
+    """
+    cdef unsigned int mStar = 0
+    cdef unsigned int i = 0
+    cdef unsigned int p, q, numOmegai, numOmegaBari, indP, indQ, t
+    cdef unsigned int m = X.shape[0]
+    cdef unsigned int n = X.shape[1]
+    cdef unsigned int s = 0
+    cdef double uivp, kappa, onePlusKappa, uivq, gamma, onePlusGamma, denom, riExp, uivpExp
+    cdef numpy.ndarray[numpy.float_t, ndim=2, mode="c"] dV = numpy.zeros((indsJ.shape[0], k), numpy.float)
+    cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] deltaAlpha = numpy.zeros(k, numpy.float)
+    cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] deltaBeta = numpy.zeros(k, numpy.float)
+    cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] ui = numpy.zeros(k, numpy.float)
+    cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] vp = numpy.zeros(k, numpy.float)
+    cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] vq = numpy.zeros(k, numpy.float)
+    cdef numpy.ndarray[numpy.uint_t, ndim=1, mode="c"] omegai = numpy.zeros(k, numpy.uint)
+    cdef numpy.ndarray[numpy.uint_t, ndim=1, mode="c"] omegaBari = numpy.zeros(k, numpy.uint)
+    cdef numpy.ndarray[numpy.uint_t, ndim=1, mode="c"] allInds = numpy.arange(n, dtype=numpy.uint)
+    cdef numpy.ndarray[numpy.int_t, ndim=1, mode="c"] indsI = numpy.zeros(k, numpy.int)
+    
+    for t in range(indsJ.shape[0]):
+        j = indsJ[t]
+        dV[t, :] = lmbda * V[j, :]
+        indsI = numpy.random.randint(0, m, sampleSize)
+        #precompute U[indsI, :].V.T
+    
+        for s in range(sampleSize): 
+            i = indsI[s]
+            omegai = omegaList[i]
+            numOmegai = omegai.shape[0]       
+            numOmegaBari = n-numOmegai
+            
+            indP = numpy.random.randint(0, omegai.shape[0])
+            
+            deltaBeta = numpy.zeros(k) 
+            ui = U[i, :]
+            riExp = exp(r[i])
+            
+            if X[i, j] != 0: 
+                #if j not in omega:
+                omegaBari = numpy.setdiff1d(allInds, omegai, assume_unique=True)
+                indQ = numpy.random.randint(0, omegaBari.shape[0])
+                
+                p = j 
+                uivp = ui.T.dot(V[p, :])
+                #uivp = UV[s, p]
+                uivpExp = exp(uivp)            
+                
+                kappa = riExp/uivpExp
+                onePlusKappaSq = (1+kappa)**2
+                
+                #Pick a random negative label
+                q = omegaBari[indQ] 
+                uivq = ui.T.dot(V[q, :])
+                gamma = exp(uivq)/uivpExp
+                
+                denom = (1+gamma)**2 * onePlusKappaSq 
+                deltaBeta += ui*((gamma+kappa+ 2*gamma*kappa)/denom)
+                deltaAlpha += deltaBeta/float(numOmegai)
+            else:
+                q = j 
+                uivq = ui.T.dot(V[q, :])
+                
+                p = omegai[indP] 
+                uivp = ui.T.dot(V[p, :])
+                uivpExp = exp(uivp)
+                
+                gamma = exp(uivq)/uivpExp
+                kappa = riExp/uivpExp
+                
+                deltaBeta -= ui* (gamma/((1+gamma)**2 * (1+kappa)))
+                deltaAlpha += deltaBeta/float(numOmegaBari)
+        
+        deltaAlpha /= float(sampleSize)
+        dV[t, :] -= deltaAlpha
+    
+    return dV
