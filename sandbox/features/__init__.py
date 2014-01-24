e82484d519deb798bd049561e3f2f9aa2d1b199a
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-24 18:29:58 +0000

    Move feature extraction stuff

--- sandbox/features/__init__.py
+++
@@ -1,4 +0,0 @@
-from sandbox.features.KernelCCA import KernelCCA
-#from apgl.features.MissingCompleter import MissingCompleter
-from sandbox.features.PrimalPCA import PrimalPCA
-from sandbox.features.PrimalCCA import PrimalCCA
