bd87c2800eb8037c457c4c24c6f7e3fa4a83c51b
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-31 13:49:34 +0100

    Test approximate updates

--- sandbox/recommendation/test/MaxLocalAUCCythonTest.py
+++ sandbox/recommendation/test/MaxLocalAUCCythonTest.py
@@ -1,6 +1,6 @@
 
 import sys
-from sandbox.recommendation.MaxLocalAUCCython import localAUCApprox, updateUApprox 
+from sandbox.recommendation.MaxLocalAUCCython import localAUCApprox, updateUApprox, updateVApprox 
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC 
 from sandbox.util.SparseUtils import SparseUtils
 import numpy
@@ -17,7 +17,8 @@ class MaxLocalAUCTest(unittest.TestCase):
         
         numpy.seterr(all="raise")
         numpy.random.seed(21)
-        
+    
+    @unittest.skip("")    
     def testLocalAucApprox(self): 
         m = 100 
         n = 200 
@@ -68,10 +69,14 @@ class MaxLocalAUCTest(unittest.TestCase):
        
     #@unittest.skip("")
     def testUpdateUApprox(self): 
+        """
+        We'll test the case in which we approximate using a large number of samples 
+        for the AUC and see if we get close to the exact derivative 
+        """
         m = 20 
         n = 30 
         k = 2 
-        numInds = 50
+        numInds = 60
         X, U, s, V = SparseUtils.generateSparseLowRank((m, n), k, numInds, verbose=True)
         
         X = X/X
@@ -80,23 +85,85 @@ class MaxLocalAUCTest(unittest.TestCase):
         lmbda = 0.0
         eps = 0.001
         maxLocalAuc = MaxLocalAUC(lmbda, k, r, eps=eps)
-        maxLocalAuc.numAucSamples = 10
-        maxLocalAuc.sigma = 10
+        maxLocalAuc.numAucSamples = m*n
+        maxLocalAuc.sigma = 1
+        maxLocalAuc.iterationsPerUpdate = m
         
         omegaList = maxLocalAuc.getOmegaList(X) 
         
         U = numpy.random.rand(X.shape[0], k)
         V = numpy.random.rand(X.shape[1], k)   
-        iterations = 20
-        
-        lastU = U.copy()        
-        
-        updateUApprox(X, U, V, omegaList, maxLocalAuc.numAucSamples, maxLocalAuc.sigma, iterations, maxLocalAuc.k, maxLocalAuc.lmbda, maxLocalAuc.r)
-        
-        #TODO: A lot more testing 
-        
 
+        lastU = U.copy()  
+        
+        #print(U)
+        #print(V)
+        rowInds = numpy.arange(m, dtype=numpy.uint)
+        updateUApprox(X, U, V, omegaList, rowInds, maxLocalAuc.numAucSamples, maxLocalAuc.sigma, maxLocalAuc.lmbda, maxLocalAuc.r)
+
+        dU = U-lastU        
+        dU = numpy.diag(1/numpy.sqrt(numpy.diag(dU.dot(dU.T)))).dot(dU)
+        #print(dU)
+        
+        #Let's compare against using the exact derivative 
+        dU2 = numpy.zeros(U.shape)
+        for i in range(X.shape[0]): 
+            dU2[i, :] = -maxLocalAuc.derivativeUi(X, lastU, V, omegaList, i)
+        
+        dU2 = numpy.diag(1/numpy.sqrt(numpy.diag(dU2.dot(dU2.T)))).dot(dU2)
+        #print(dU2)
+        
+        
+        similarities = numpy.diag(dU.dot(dU2.T))
+        nptst.assert_array_almost_equal(similarities, numpy.ones(m), 2)
+        
+        
+    #@unittest.skip("")
+    def testUpdateVApprox(self): 
+        """
+        We'll test the case in which we approximate using a large number of samples 
+        for the AUC and see if we get close to the exact derivative 
+        """
+        m = 20 
+        n = 30 
+        k = 2 
+        numInds = 60
+        X, U, s, V = SparseUtils.generateSparseLowRank((m, n), k, numInds, verbose=True)
+        
+        X = X/X
+
+        r = numpy.ones(m)*0
+        lmbda = 0.0
+        eps = 0.001
+        maxLocalAuc = MaxLocalAUC(lmbda, k, r, eps=eps)
+        maxLocalAuc.numAucSamples = m*n
+        maxLocalAuc.sigma = 1
+        maxLocalAuc.iterationsPerUpdate = m
+        
+        omegaList = maxLocalAuc.getOmegaList(X) 
+        
+        U = numpy.random.rand(X.shape[0], k)
+        V = numpy.random.rand(X.shape[1], k)   
+
+         
+        #Lets compute derivatives col by col
+        for i in range(n): 
+            lastV = V.copy() 
             
+            colInds = numpy.array([i], numpy.uint)
+            rowInds = numpy.arange(m, dtype=numpy.uint)
+            updateVApprox(X, U, V, omegaList, rowInds, colInds, maxLocalAuc.numAucSamples, maxLocalAuc.sigma, maxLocalAuc.lmbda, maxLocalAuc.r) 
+
+            dV = V[i, :] - lastV[i, :]   
+            dV2 = -maxLocalAuc.derivativeVi(X, U, lastV, omegaList, i)
+
+            dV = dV/numpy.linalg.norm(dV)
+            dV2 = dV2/numpy.linalg.norm(dV2)
+
+            similarity = dV.dot(dV2)
+            
+            self.assertAlmostEquals(similarity, 1, 3)
+      
     
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
