f078fec09a3a162b256488f6633a808b9e5206f0
Romaric Gaudel <romaric.gaudel@univ-lille3.fr>
2014-04-14 21:54:40 +0200

    model selection for CLiMF

--- sandbox/recommendation/CLiMF.py
+++ sandbox/recommendation/CLiMF.py
@@ -5,9 +5,15 @@ Wrapper to use gamboviol implementation of CLiMF.
 import numpy 
 import sppy
 import logging
+import multiprocessing
 from sandbox.util.MCEvaluator import MCEvaluator 
 from sandbox.util.PathDefaults import PathDefaults
 from sandbox.util.Sampling import Sampling
+from sandbox.util.SparseUtils import SparseUtils
+from sandbox.recommendation.MaxLocalAUCCython import localAUCApprox
+from sandbox.util.SparseUtilsCython import SparseUtilsCython
+
+import itertools
 
 try:
     from climf import update
@@ -17,8 +23,19 @@ except:
         raise NameError("'climf' is not installed, so 'update' is not defined")
         
         
+def computeTestAucs(args): 
+    trainX, testX, testOmegaList, maxLocalAuc, w, numRecordAucSamples = args 
+    logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
+    
+    maxLocalAuc.learnModel(trainX.toScipyCsr(), U=maxLocalAuc.U, V=maxLocalAuc.V)
+    r = SparseUtilsCython.computeR(maxLocalAuc.U, maxLocalAuc.V, w, numRecordAucSamples) 
+    localAUC = localAUCApprox(testX, maxLocalAuc.U, maxLocalAuc.V, testOmegaList, numRecordAucSamples, r)
+    
+    logging.debug("local AUC: " + str(localAUC) + " with k=" + str(maxLocalAuc.k) + " lmbda=" + str(maxLocalAuc.lmbda) + " gamma=" + str(maxLocalAuc.gamma))
+        
+    return localAUC
 
-class Climf(object): 
+class CLiMF(object): 
     """
     An interface to use CLiMF recommender system. 
     """
@@ -30,19 +47,100 @@ class Climf(object):
                 
         self.max_iters = 25
         
-    def learnModel(self, X): 
-        self.X=X
-        self.U = 0.01*numpy.random.random_sample((X.shape[0],self.k))
-        self.V = 0.01*numpy.random.random_sample((X.shape[1],self.k))
+        #Model selection parameters 
+        self.folds = 5 
+        self.testSize = 3
+        self.ks = 2**numpy.arange(3, 8)
+        self.lmbdas = 2.0**-numpy.arange(1, 20, 4)
+        self.gammas = 2.0**-numpy.arange(1, 20, 4)
+        self.numRecordAucSamples = 500
+        self.w = 0.1
+        self.numProcesses = multiprocessing.cpu_count()
+        self.chunkSize = 1
+        
+    def initUV(self, X, k=None):
+        if k == None:
+            k=self.k 
+        U = 0.01*numpy.random.random_sample((X.shape[0],k))
+        V = 0.01*numpy.random.random_sample((X.shape[1],k))
+        return U,V
 
+    def learnModel(self, X, U=None, V=None):
+        if U==None or V==None:
+            self.U, self.V = self.initUV(X)
+        else:
+            self.U = U
+            self.V = V
         for it in xrange(self.max_iters):
             logging.debug("Iteration " + str(it))
-            update(self.X,self.U,self.V,self.lmbda,self.gamma)
+            update(X,self.U,self.V,self.lmbda,self.gamma)
     
     def predict(self, maxItems): 
         return MCEvaluator.recommendAtk(self.U, self.V, maxItems)
     
-    def modelSelect(self, X): 
+    def modelSelect(self, X):
+        """
+        Perform model selection on X and return the best parameters. 
+        """
+        m, n = X.shape
+        #cvInds = Sampling.randCrossValidation(self.folds, X.nnz)
+        trainTestXs = Sampling.shuffleSplitRows(X, self.folds, self.testSize)
+        datas = []
+        for (trainX, testX) in trainTestXs:
+            testOmegaList = SparseUtils.getOmegaList(testX)
+            testX = trainX+testX
+            datas.append((trainX, testX, testOmegaList))
+        testAucs = numpy.zeros((self.ks.shape[0], self.lmbdas.shape[0], self.gammas.shape[0], len(trainTestXs)))
+        
+        logging.debug("Performing model selection")
+        paramList = []        
+        
+        for i, k in enumerate(self.ks): 
+            U, V = self.initUV(X, k)
+            for lmbda in self.lmbdas:
+                for gamma in self.gammas:
+                    for (trainX, testX, testOmegaList) in datas:
+                        maxLocalAuc = self.copy()
+                        maxLocalAuc.k = k
+                        maxLocalAuc.U = U.copy()
+                        maxLocalAuc.V = V.copy()
+                        maxLocalAuc.lmbda = lmbda
+                        maxLocalAuc.gamma = gamma
+                    
+                        paramList.append((trainX, testX, testOmegaList, maxLocalAuc, self.w, self.numRecordAucSamples))
+            
+        if self.numProcesses != 1: 
+            pool = multiprocessing.Pool(processes=self.numProcesses, maxtasksperchild=100)
+            resultsIterator = pool.imap(computeTestAucs, paramList, self.chunkSize)
+        else: 
+            resultsIterator = itertools.imap(computeTestAucs, paramList)
+        
+        for i_k in range(len(self.ks)):
+            for i_lmbda in range(len(self.lmbdas)):
+                for i_gamma in range(len(self.gammas)):
+                    for i_cv in range(len(trainTestXs)):             
+                        testAucs[i_k, i_lmbda, i_gamma, i_cv] = resultsIterator.next()
+        
+        if self.numProcesses != 1: 
+            pool.terminate()
+        
+        meanTestLocalAucs = numpy.mean(testAucs, 3)
+        stdTestLocalAucs = numpy.std(testAucs, 3)
+        
+        logging.debug("ks=" + str(self.ks))
+        logging.debug("lmbdas=" + str(self.lmbdas))
+        logging.debug("gammas=" + str(self.gammas))
+        logging.debug("Mean local AUCs=" + str(meanTestLocalAucs))
+        
+        i_k, i_lmbda, i_gamma = numpy.unravel_index(meanTestLocalAucs.argmax(), meanTestLocalAucs.shape)
+        self.k = self.ks[i_k]
+        self.lmbda = self.lmbdas[i_lmbda]
+        self.gamma = self.gammas[i_gamma]
+
+        logging.debug("Model parameters: k=" + str(self.k) + " lmbda=" + str(self.lmbda) + " gamma=" + str(self.gamma))
+         
+        return meanTestLocalAucs, stdTestLocalAucs
+
         """
         Perform model selection on X and return the best parameters. 
         """
