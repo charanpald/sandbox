7048953302dbae4fdf0eda86c9ca4488978e6753
Charanpal Dhanjal <charanpal@gmail.com>
2014-05-23 15:51:43 +0200

    New file

---
+++ sandbox/recommendation/profile/MaxLocalAUCCythonProfile.py
@@ -0,0 +1,54 @@
+import numpy
+import logging
+import sys
+from sandbox.util.ProfileUtils import ProfileUtils
+from sandbox.recommendation.MaxLocalAUCCython import derivativeViApprox
+from sandbox.util.SparseUtils import SparseUtils
+from sandbox.util.MCEvaluator import MCEvaluator
+from sandbox.util.Sampling import Sampling
+from sandbox.util.SparseUtilsCython import SparseUtilsCython
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+class MaxLocalAUCCythonProfile(object):
+    def __init__(self):
+        numpy.random.seed(21)        
+        
+        #Create a low rank matrix  
+        self.m = 1000 
+        self.n = 500 
+        self.k = 10 
+        self.X = SparseUtils.generateSparseBinaryMatrix((self.m, self.n), self.k, csarray=True)
+        
+        
+    def profileDerivativeVjApprox(self):
+        k = 10
+        U = numpy.random.rand(self.m, k)
+        V = numpy.random.rand(self.n, k)
+        
+        indPtr, colInds = SparseUtils.getOmegaListPtr(self.X)  
+        colIndsProbabilities = numpy.ones(colInds.shape[0])
+        
+        for i in range(self.m): 
+            colIndsProbabilities[indPtr[i]:indPtr[i+1]] /= colIndsProbabilities[indPtr[i]:indPtr[i+1]].sum()
+            colIndsProbabilities[indPtr[i]:indPtr[i+1]] = numpy.cumsum(colIndsProbabilities[indPtr[i]:indPtr[i+1]])
+        
+        j = 3
+        numRowSamples = 100
+        numAucSamples = 10
+        
+        xi = numpy.zeros(self.m)
+        lmbda = 0.001
+        C = 0 
+        normalise = True
+        
+        def run(): 
+            numRuns = 1
+            for i in range(numRuns): 
+                for j in range(self.n):     
+                    derivativeViApprox(indPtr, colInds,  U,  V,  colIndsProbabilities, j, numRowSamples, numAucSamples, xi, lmbda, C, normalise)
+                
+        ProfileUtils.profile('run()', globals(), locals())
+
+profiler = MaxLocalAUCCythonProfile()
+profiler.profileDerivativeVjApprox()
