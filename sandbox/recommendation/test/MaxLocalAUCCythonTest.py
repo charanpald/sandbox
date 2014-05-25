fc0208d436bc5083cc8ef8c8dff5d66a491939c8
Charanpal Dhanjal <charanpal@gmail.com>
2014-05-25 18:01:23 +0200

    Move out irrelevant test cases

--- sandbox/recommendation/test/MaxLocalAUCCythonTest.py
+++ sandbox/recommendation/test/MaxLocalAUCCythonTest.py
@@ -1,6 +1,6 @@
 
 import sys
-from sandbox.recommendation.MaxLocalAUCCython import localAUCApprox, derivativeUiApprox, derivativeUi, derivativeViApprox, derivativeVi, inverseChoicePy, choicePy
+from sandbox.recommendation.MaxLocalAUCCython import derivativeUiApprox, derivativeUi, derivativeViApprox, derivativeVi
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC 
 from sandbox.util.SparseUtils import SparseUtils
 from sandbox.util.SparseUtilsCython import SparseUtilsCython 
@@ -17,55 +17,7 @@ class MaxLocalAUCTest(unittest.TestCase):
         
         numpy.seterr(all="raise")
         numpy.random.seed(21)
-    
-    @unittest.skip("")    
-    def testLocalAucApprox(self): 
-        m = 100 
-        n = 200 
-        k = 2 
-        numInds = 100
-        X, U, s, V = SparseUtils.generateSparseLowRank((m, n), k, numInds, verbose=True)
-        
-        X = X/X
 
-        r = numpy.ones(m)*-10
-        lmbda = 0.0
-        eps = 0.001
-        maxLocalAuc = MaxLocalAUC(lmbda, k, r, eps=eps)
-        
-        omegaList = maxLocalAuc.getOmegaList(X)
-        
-        localAuc = maxLocalAuc.localAUC(X, U, V, omegaList)
-        
-        samples = numpy.arange(50, 200, 10)
-        
-        for i, sampleSize in enumerate(samples): 
-            maxLocalAuc.numAucSamples = sampleSize
-            localAuc2 = localAUCApprox(X, U, V, omegaList, sampleSize, r)
-
-            self.assertAlmostEqual(localAuc2, localAuc, 1)
-        
-        #Test more accurately 
-        sampleSize = 1000
-        localAuc2 = localAUCApprox(X, U, V, omegaList, sampleSize, r)
-        self.assertAlmostEqual(localAuc2, localAuc, 2)
-        
-        #Now set a high r 
-        Z = U.dot(V.T)
-        r = numpy.ones(m)*0
-        maxLocalAuc.r = r
-        localAuc = maxLocalAuc.localAUC(X, U, V, omegaList)   
-
-        for i, sampleSize in enumerate(samples): 
-            maxLocalAuc.numAucSamples = sampleSize
-            localAuc2 = localAUCApprox(X, U, V, omegaList, sampleSize, r)
-
-            self.assertAlmostEqual(localAuc2, localAuc, 1)
-            
-        #Test more accurately 
-        sampleSize = 1000
-        localAuc2 = localAUCApprox(X, U, V, omegaList, sampleSize, r)
-        self.assertAlmostEqual(localAuc2, localAuc, 2)
        
     @unittest.skip("")
     def testDerivativeUiApprox(self): 
@@ -222,43 +174,7 @@ class MaxLocalAUCTest(unittest.TestCase):
             nptst.assert_array_almost_equal(dv1, dv2, 3)
 
 
-    def testInverseChoicePy(self):
-        n = 100
-        a = numpy.array(numpy.random.randint(0, n, 50), numpy.int32)
-        a = numpy.unique(a)
 
-        numRuns = 100 
-        for i in range(numRuns): 
-            j = inverseChoicePy(a, n)
-            self.assertTrue(j not in a)
-        
-
-    def testChoicePy(self): 
-        n = 100
-        k = 50
-        a = numpy.array(numpy.random.randint(0, n, k), numpy.int32)
-        a = numpy.unique(a)
-        probs = numpy.ones(a.shape[0])/float(a.shape[0])
-        
-        sample = choicePy(a, 10, probs)
-
-        for item in sample:
-            self.assertTrue(item in a)
-
-        probs = numpy.zeros(a.shape[0])
-        probs[2] = 1
-        sample = choicePy(a, 10, probs)
-        
-        for item in sample:
-            self.assertEquals(item, a[2])
-            
-        a = numpy.array([0, 1, 2], numpy.int32)
-        probs = numpy.array([0.2, 0.6, 0.2])
-        
-        runs = 10000
-        sample = choicePy(a, runs, probs)
-        
-        nptst.assert_array_almost_equal(numpy.bincount(sample)/float(runs), probs, 2)
         
 
 if __name__ == "__main__":
