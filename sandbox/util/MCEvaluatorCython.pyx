a436ad65b342b0d25b9989517ea26b36e386dd40
Charanpal Dhanjal <charanpal@gmail.com>
2014-05-25 18:01:55 +0200

    Add local AUC approx

--- sandbox/util/MCEvaluatorCython.pyx
+++ sandbox/util/MCEvaluatorCython.pyx
@@ -6,6 +6,7 @@ import numpy
 cimport numpy
 import scipy.sparse 
 numpy.import_array()
+from sandbox.util.CythonUtils cimport dot, scale, choice, inverseChoice, uniformChoice, plusEquals
 
 
 class MCEvaluatorCython(object):
@@ -81,3 +82,44 @@ class MCEvaluatorCython(object):
                 recalls[i] = count/omegai.shape[0]
         
         return recalls  
+        
+    @staticmethod
+    def localAUCApprox(numpy.ndarray[int, ndim=1, mode="c"] indPtr, numpy.ndarray[int, ndim=1, mode="c"] colInds, numpy.ndarray[int, ndim=1, mode="c"] allIndPtr, numpy.ndarray[int, ndim=1, mode="c"] allColInds, numpy.ndarray[double, ndim=2, mode="c"] U, numpy.ndarray[double, ndim=2, mode="c"] V, unsigned int numAucSamples, numpy.ndarray[double, ndim=1, mode="c"] r): 
+        """
+        Compute the estimated local AUC for the score functions UV^T relative to a matrix 
+        X with quantile vector r. If evaluating on a set of test observations then (indPtr, colInds)
+        is the test set and (allIndPtr, allColInds) is all positive elements. 
+        """
+        cdef unsigned int m = U.shape[0]
+        cdef unsigned int n = V.shape[0]
+        cdef numpy.ndarray[int, ndim=1, mode="c"] omegai 
+        cdef numpy.ndarray[int, ndim=1, mode="c"] allOmegai 
+        cdef numpy.ndarray[int, ndim=1, mode="c"] omegaiSample
+        cdef numpy.ndarray[numpy.float_t, ndim=1, mode="c"] localAucArr = numpy.zeros(m)
+        cdef unsigned int i, j, k, ind, p, q, nOmegai
+        cdef double partialAuc, ri, uivp
+    
+        k = U.shape[1]
+    
+        for i in range(m): 
+            omegai = colInds[indPtr[i]:indPtr[i+1]]
+            allOmegai = allColInds[allIndPtr[i]:allIndPtr[i+1]]
+            nOmegai = omegai.shape[0]
+            ri = r[i]
+            
+            if nOmegai * (n-nOmegai) != 0: 
+                partialAuc = 0    
+                #omegaiSample = uniformChoice(omegai, numAucSamples)  
+                #omegaiSample = numpy.random.choice(omegai, numAucSamples)
+                omegaiSample = omegai
+                
+                for p in omegaiSample:                
+                    q = inverseChoice(allOmegai, n)                
+                    uivp = dot(U, i, V, p, k)
+    
+                    if uivp > ri and uivp > dot(U, i, V, q, k): 
+                        partialAuc += 1 
+                            
+                localAucArr[i] = partialAuc/float(omegaiSample.shape[0])     
+        
+        return localAucArr.mean()
