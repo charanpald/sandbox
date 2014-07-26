56a416561aa2bd81a9e7437cf9d814cddead69f6
Charanpal Dhanjal <charanpal@gmail.com>
2014-07-26 15:02:45 +0100

    Tidy up and test parallel learning

--- sandbox/recommendation/test/MaxLocalAUCTest.py
+++ sandbox/recommendation/test/MaxLocalAUCTest.py
@@ -17,34 +17,31 @@ class MaxLocalAUCTest(unittest.TestCase):
         numpy.seterr(all="raise")
         numpy.random.seed(22)
     
-    #@unittest.skip("")
+    @unittest.skip("")
     def testLearnModel(self): 
         m = 50 
         n = 20 
         k = 5 
         X = SparseUtils.generateSparseBinaryMatrix((m, n), k, csarray=True)
 
-        
         u = 0.1
         w = 1-u
         eps = 0.05
         
         maxLocalAuc = MaxLocalAUC(k, w, alpha=5.0, eps=eps)
-        
         #U, V = maxLocalAuc.learnModel(X)
-        
         
         maxLocalAuc.stochastic = True 
         U, V = maxLocalAuc.learnModel(X)
-        #print(U)
-        #print(V)
         
-        maxLocalAuc.itemFactors = True 
+        #Test case where we do not have validation set 
+        maxLocalAuc.validationUsers = 0.0
         U, V = maxLocalAuc.learnModel(X)
 
 
-    @unittest.skip("")
+    #@unittest.skip("")
     def testParallelLearnModel(self): 
+        numpy.random.seed(21)
         m = 50 
         n = 20 
         k = 5 
@@ -54,8 +51,12 @@ class MaxLocalAUCTest(unittest.TestCase):
         u = 0.1
         w = 1-u
         eps = 0.05
-        maxLocalAuc = MaxLocalAUC(k, w, alpha=5.0, eps=eps, stochastic=True)
-        
+        maxLocalAuc = MaxLocalAUC(k, w, alpha=1.0, eps=eps, stochastic=True)
+        maxLocalAuc.maxIterations = 3
+        maxLocalAuc.recordStep = 1
+        maxLocalAuc.rate = "optimal"
+        maxLocalAuc.t0 = 2.0
+        maxLocalAuc.validationUsers = 0.0
         
         os.system('taskset -p 0xffffffff %d' % os.getpid())
         maxLocalAuc.parallelSGD = True
@@ -114,7 +115,6 @@ class MaxLocalAUCTest(unittest.TestCase):
         eps = 0.001
         maxLocalAuc = MaxLocalAUC(k, u, eps=eps)    
         
-        print(maxLocalAuc)
 
     def testCopy(self): 
         u= 0.1
@@ -123,108 +123,9 @@ class MaxLocalAUCTest(unittest.TestCase):
         maxLocalAuc = MaxLocalAUC(k, u, alpha=5.0, eps=eps)
         maxLocalAuc.copy()
 
-    def testOmegaProbsUniform(self):
-        m = 10 
-        n = 20
-        p  = 5
-        X = SparseUtils.generateSparseBinaryMatrix((m, n), p, csarray=True)
-        
-        indPtr, colInds = SparseUtils.getOmegaListPtr(X)
 
-        u= 0.1
-        eps = 0.001
-        k = 10 
-        maxLocalAuc = MaxLocalAUC(k, u, alpha=5.0, eps=eps)
-        
-        U, V = maxLocalAuc.initUV(X)
-        
-        colIndsCumProbs = maxLocalAuc.omegaProbsUniform(indPtr, colInds, U, V)
-        
-        for i in range(m): 
-            omegai = colInds[indPtr[i]:indPtr[i+1]]
-            omegaiProbs = colIndsCumProbs[indPtr[i]:indPtr[i+1]]
-            self.assertAlmostEquals(omegaiProbs[-1], 1)
-            
-            probs = numpy.ones(omegai.shape[0])
-            probs /= probs.sum()
-            probs = numpy.cumsum(probs)
-            nptst.assert_array_almost_equal(probs, omegaiProbs)
         
 
-    def testOmegaProbsTopZ(self):
-        m = 10 
-        n = 20
-        u = 0.5
-        w = 1-u
-        p  = 5
-        X = SparseUtils.generateSparseBinaryMatrix((m, n), p, w, csarray=True)
-        
-        indPtr, colInds = SparseUtils.getOmegaListPtr(X)
-        
-        X2 = numpy.zeros((m, n))
-        for i in range(m): 
-            omegai = colInds[indPtr[i]:indPtr[i+1]]
-            X2[i, omegai] = 1
-            
-        nptst.assert_array_equal(X.toarray(), X2)
-
-        eps = 0.001
-        k = 10 
-        maxLocalAuc = MaxLocalAUC(k, w, alpha=5.0, eps=eps)
-        
-        U, V = maxLocalAuc.initUV(X)
-        
-        colIndsCumProbs = maxLocalAuc.omegaProbsTopZ(indPtr, colInds, U, V)
-        
-        Z = U.dot(V.T)
-        
-        for i in range(m): 
-            omegai = colInds[indPtr[i]:indPtr[i+1]]
-            omegaiProbs = colIndsCumProbs[indPtr[i]:indPtr[i+1]]
-            self.assertAlmostEquals(omegaiProbs[-1], 1)
-            
-            vals = Z[i, omegai]
-            
-            sortedVals = numpy.flipud(numpy.sort(vals))
-            ri = sortedVals[min(maxLocalAuc.recommendSize, sortedVals.shape[0])-1]
-            probs = numpy.zeros(omegai.shape[0])
-            probs[vals >= ri] = 1
-            probs /= probs.sum()
-            
-            probs = numpy.cumsum(probs)
-            nptst.assert_array_almost_equal(probs, omegaiProbs)
-
-
-    def testOmegaProbsRank(self):
-        m = 10 
-        n = 20
-        p  = 5
-        X = SparseUtils.generateSparseBinaryMatrix((m, n), p, csarray=True)
-        
-        indPtr, colInds = SparseUtils.getOmegaListPtr(X)
-
-        u= 0.1
-        eps = 0.001
-        k = 10 
-        maxLocalAuc = MaxLocalAUC(k, u, alpha=5.0, eps=eps)
-        
-        U, V = maxLocalAuc.initUV(X)
-        
-        colIndsCumProbs = maxLocalAuc.omegaProbsRank(indPtr, colInds, U, V)
-        
-        Z = U.dot(V.T)
-        
-        for i in range(m): 
-            omegai = colInds[indPtr[i]:indPtr[i+1]]
-            omegaiProbs = colIndsCumProbs[indPtr[i]:indPtr[i+1]]
-            self.assertAlmostEquals(omegaiProbs[-1], 1)
-            
-            vals = Z[i, omegai]
-            inds = numpy.argsort(numpy.argsort((vals)))+1
-
-            probs = inds/float(inds.sum())
-            probs = numpy.cumsum(probs)
-            nptst.assert_array_almost_equal(probs, omegaiProbs)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
