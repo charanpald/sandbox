086f7e16ae6696a42fe657432500dadc57a28942
Charanpal Dhanjal <charanpal@gmail.com>
2014-09-22 15:00:00 +0200

    Profile sample users

--- sandbox/util/profile/SamplingProfile.py
+++ sandbox/util/profile/SamplingProfile.py
@@ -23,6 +23,21 @@ class SamplingProfile(object):
 
         ProfileUtils.profile('Sampling.shuffleSplitRows(X, k2, testSize)', globals(), locals())
         
+    def profileSampleUsers(self): 
+        m = 10000
+        n = 50000
+        k = 5 
+        u = 0.01
+        w = 1-u
+        X, U, s, V, wv  = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
+
+        print(X.nnz)
+
+        k2 = 100000
+
+        ProfileUtils.profile('Sampling.sampleUsers2(X, k2)', globals(), locals())      
+        
 if __name__ == '__main__':     
     profiler = SamplingProfile()
-    profiler.profileShuffleSplitRows() 
+    #profiler.profileShuffleSplitRows() 
+    profiler.profileSampleUsers() 
